be264be75a0a5c33d7873c99c34ee6b8892c00ad
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-08 11:10:12 -0400

    submenu hidden

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -11,6 +11,8 @@ import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } fro
 export class Dashboard {
   private readonly router = inject(Router);
   isSidebarOpen = signal(false);
+  isSubBarOpen = signal(false);
+  
   user = signal({ name: 'Admin User', avatar: 'https://i.pravatar.cc/150?u=admin' });
   rate = signal<number>(3800);
 
@@ -19,11 +21,18 @@ export class Dashboard {
       if (event instanceof NavigationEnd && this.isSidebarOpen()) {
         this.isSidebarOpen.set(false);
       }
+      if (event instanceof NavigationEnd && this.isSubBarOpen()) {
+        this.isSubBarOpen.set(false);
+      }
     });
   }
 
   toggleSidebar() {
     this.isSidebarOpen.update((isOpen) => !isOpen);
+  }
+
+  toggleSubBar() {
+    this.isSubBarOpen.update((isOpen) => !isOpen);
   }
 
   updateRate(): void {
