ce138f3c6a5e264805b52297635b8abf1b4cf2ec
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-08 14:15:03 -0400

    planes

--- src/app/pages/dashboard/plans/plans.ts
+++ src/app/pages/dashboard/plans/plans.ts
@@ -1,9 +1,16 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-plans',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './plans.html',
   styleUrl: './plans.css',
 })
-export class Plans {}
+export class Plans {
+  plans = [
+    {name: 'Basic', amount: 15, bonus: 2, status: 'active'},
+    {name: 'Pro', amount: 30, bonus: 5, status: 'active'},
+    {name: 'Enterprise', amount: 50, bonus: 7, status: 'active'}
+  ]
+}
