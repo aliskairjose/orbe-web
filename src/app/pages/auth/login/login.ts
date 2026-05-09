cd84f8ce5bbdb03e2dc3c7a847ca7354f69c366c
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-09 09:51:56 -0400

    form signal

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -3,57 +3,45 @@ import { NgIf } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { Store } from '@ngxs/store';
+import { AuthActions } from '../store/auth.actions';
+import { ERoutes } from '@core/enums';
+import {email, form, FormField, required} from '@angular/forms/signals';
 
+interface LoginData {
+  email: string;
+  password: string;
+}
+const loginModel = signal<LoginData>({
+  email: '',
+  password: '',
+});
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   private readonly fb = inject(FormBuilder);
+  private readonly store = inject(Store);
   private readonly router = inject(Router);
-  private readonly authService = inject(AuthService);
 
-  protected readonly loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+  form = form(loginModel, (schemaPath)=>{
+    required(schemaPath.email, {message: 'Email is required'});
+    email(schemaPath.email, {message: 'Enter a valid email address'});
+    required(schemaPath.password, {message: 'Password is required'});
   });
 
-  protected readonly loginError = signal(false);
 
-  private readonly fakeCredentials = {
-    email: 'user@example.com',
-    password: 'password123',
-  };
-
-  protected get emailControl() {
-    return this.loginForm.get('email');
-  }
-
-  protected get passwordControl() {
-    return this.loginForm.get('password');
-  }
-
-  protected submit(): void {
-    this.loginError.set(false);
-
-    if (this.loginForm.invalid) {
-      this.loginForm.markAllAsTouched();
-      return;
-    }
-
-    const { email, password } = this.loginForm.value as {
-      email: string;
-      password: string;
-    };
-
-    if (email === this.fakeCredentials.email && password === this.fakeCredentials.password) {
-      this.authService.login();
-      this.router.navigate(['/dashboard/home']);
-      return;
-    }
-
-    this.loginError.set(true);
+   onSubmit(event: Event) {
+    event.preventDefault();
+    const credentials = loginModel();
+    console.log('Logging in with:', credentials);
+     this.store.dispatch(new AuthActions.Login(credentials)).subscribe(() => {
+      // this.#socketService.connect();
+      // this.#toastService.show(MessageEnum.Welcome);
+      this.router.navigate([ERoutes.Dashboard]);
+    });
   }
 }
