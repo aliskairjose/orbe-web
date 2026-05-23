56ed88e3ff2ae164f28b3bf2d7c4250dc6829ddf
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-23 09:24:21 -0400

    landing

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { AuthSelectors } from '../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-landing',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
-export class Landing { 
+export class Landing {
   baseUrl = API_URL;
   env = ENV;
+
+  private store = inject(Store);
+
+  protected readonly accessButtonTitle = this.store.selectSnapshot(AuthSelectors.isAuthenticated)
+    ? 'Dashboard'
+    : 'Iniciar sesión';
 }
