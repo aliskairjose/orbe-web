82dd5e62ed1099faaccac8c0cbc0d777763a6abe
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-27 12:24:08 -0400

    lista de chats

--- src/app/pages/dashboard/chats/chats.ts
+++ src/app/pages/dashboard/chats/chats.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { httpResource } from '@angular/common/http';
+import { Component, computed, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ITEM_PER_PAGE } from '@core/constants';
+import { IResponse, IRoom } from '@core/interfaces';
 
 @Component({
   selector: 'app-chats',
-  imports: [],
+  imports: [DatePipe, FormsModule],
   templateUrl: './chats.html',
   styleUrl: './chats.css',
 })
-export class Chats {}
+export class Chats {
+  protected headers = ['cliente', 'Asesor','Mensajes','Fecha', ''];
+   private readonly url = `${API_URL}/v1/chat/rooms`;
+  protected readonly itemsPerPage = [5, 10, 15, 20];
+  protected selected = 20;
+
+  protected limit = signal(ITEM_PER_PAGE);
+  protected page = signal(1);
+  protected search = model<string>('');
+
+   resource = httpResource<IResponse<IRoom>>(() => ({
+    url: this.url,
+    params: {
+      limit: this.limit(),
+      page: this.page(),
+      search: this.search() ?? '',
+    },
+  }));
+
+  fromPage = computed(() => this.limit() * (this.page() - 1) + 1);
+
+  toPage = computed(() => {
+    return this.resource.value()?.metadata
+      ? this.limit() * (this.page() - 1) + this.resource.value()!.metadata!.resultsLength
+      : 0;
+  });
+
+  onPageChange({ value }: any): void {
+    this.selected = value;
+    this.limit.set(value);
+    this.page.set(1);
+  }
+
+  goTopage(page: number): void {
+    this.page.set(page);
+  }
+}
