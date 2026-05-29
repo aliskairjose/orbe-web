35cbbbb32b9ae11a65183895e04ac36f8351ae55
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-29 16:39:35 -0400

    redireccion

--- src/app/pages/dashboard/chats/chats.ts
+++ src/app/pages/dashboard/chats/chats.ts
@@ -2,12 +2,13 @@ import { DatePipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
 import { Component, computed, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { ITEM_PER_PAGE } from '@core/constants';
 import { IResponse, IRoom } from '@core/interfaces';
 
 @Component({
   selector: 'app-chats',
-  imports: [DatePipe, FormsModule],
+  imports: [DatePipe, FormsModule, RouterLink],
   templateUrl: './chats.html',
   styleUrl: './chats.css',
 })
