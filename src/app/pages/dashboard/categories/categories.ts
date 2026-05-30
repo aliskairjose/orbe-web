7396f8f6e127fe319fbb5eca3c718e3addbac58b
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-05-30 10:50:06 -0400

    form

--- src/app/pages/dashboard/categories/categories.ts
+++ src/app/pages/dashboard/categories/categories.ts
@@ -1,18 +1,34 @@
 import { DatePipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component, model, signal, computed } from '@angular/core';
+import { Component, model, signal, computed, inject, DOCUMENT } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormField, FormRoot, required, form } from '@angular/forms/signals';
 import { ITEM_PER_PAGE } from '@core/constants';
 import { ICategory } from '@core/interfaces';
 import { IResponse } from '@core/interfaces/response';
+import { HSOverlay } from 'flyonui/flyonui';
 
+interface Data {
+  name: string;
+  isActive: boolean;
+}
+const Model = signal<Data>({
+  name: '',
+  isActive: false,
+});
 @Component({
   selector: 'app-categories',
-  imports: [FormsModule, DatePipe],
+  imports: [FormsModule, DatePipe, FormField, FormRoot],
   templateUrl: './categories.html',
   styleUrl: './categories.css',
 })
 export class Categories {
+  private readonly document = inject(DOCUMENT);
+
+  protected form = form(Model, (validator) => {
+    required(validator.name, { message: 'El nombre es requerido' });
+  });
+
   private readonly url = `${API_URL}/v1/categories`;
   protected readonly itemsPerPage = [5, 10, 15, 20];
   protected selected = 20;
@@ -30,6 +46,8 @@ export class Categories {
     },
   }));
 
+  protected selectedCategory: ICategory | null = null;
+
   fromPage = computed(() => this.limit() * (this.page() - 1) + 1);
 
   toPage = computed(() => {
@@ -46,5 +64,22 @@ export class Categories {
 
   goTopage(page: number): void {
     this.page.set(page);
+  }
+
+  async onSubmit(planData: Data): Promise<void> {
+    if (!this.selectedCategory) return;
+    // this.service.update(this.selectedCategory._id, planData).subscribe((_) => window.location.reload());
+  }
+
+  openModal(category: ICategory): void {
+    this.selectedCategory = category;
+    Model.set({ name: category.name, isActive: category.isActive });
+    const modal = new HSOverlay(this.document.querySelector('#update-form-modal')!);
+    modal.open();
+  }
+
+  closeModal(): void {
+    const modal = new HSOverlay(this.document.querySelector('#update-form-modal')!);
+    modal.close();
   }
 }
