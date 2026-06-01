876ddbf7a56d1346665e9b7fac8d9ff78415fb11
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-06-01 15:25:10 -0400

    slied

--- src/app/pages/dashboard/users/details/details.ts
+++ src/app/pages/dashboard/users/details/details.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
 import { AnnualTimeChart, RateStats } from '@core/components';
 import { EConnectStatus } from '@core/enums';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-details',
   imports: [CommonModule, AnnualTimeChart, RateStats],
@@ -13,7 +15,40 @@ export class Details {
   @Input() id!: string;
   @Input() role!: string;
 
-  protected readonly connectStatus = EConnectStatus;
+  private sanitizer = inject(DomSanitizer);
 
+  protected readonly connectStatus = EConnectStatus;
   protected user = httpResource<any>(() => `${API_URL}/v1/users/${this.id}`);
+
+  currentIndex = signal<number>(0);
+
+  slides = computed(() => [
+    {
+      url: this.sanitizer.bypassSecurityTrustUrl(this.user.value().advisor.dniImage),
+      alt: 'DNI',
+      title: 'DNI',
+      description: 'DNI del asesor.',
+    },
+    {
+      url: this.sanitizer.bypassSecurityTrustUrl(this.user.value().advisor.dniImage),
+      alt: 'DNI',
+      title: 'DNI',
+      description: 'DNI del asesor.',
+    },
+  ]);
+
+  // Computed para verificar los límites de manera limpia
+  totalSlides = computed(() => this.slides().length);
+
+  nextSlide(): void {
+    this.currentIndex.update((index) => (index + 1) % this.totalSlides());
+  }
+
+  prevSlide(): void {
+    this.currentIndex.update((index) => (index - 1 + this.totalSlides()) % this.totalSlides());
+  }
+
+  goToSlide(index: number): void {
+    this.currentIndex.set(index);
+  }
 }
