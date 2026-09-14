df039325888e2916346fe97ca59d5ad69d39f5fd
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-09-14 16:16:55 -0400

    esotericos

--- src/app/core/landing/sections/discover/discover.ts
+++ src/app/core/landing/sections/discover/discover.ts
@@ -6,4 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './discover.html',
   styleUrl: './discover.css',
 })
-export class Discover {}
+export class Discover {
+  protected categories = [
+    {
+      'label': 'Amor',
+      'imagen':'amor'
+    },
+    {
+      'label': 'Dinero y prosperidad',
+      'imagen':'dinero_prosperidad'
+    },
+    {
+      'label': 'Tarot',
+      'imagen':'tarot'
+    },
+    {
+      'label': 'Videncia',
+      'imagen':'videncia'
+    },
+    {
+      'label': 'Energía',
+      'imagen':'energia'
+    },
+    {
+      'label': 'Limpiezas espirituales',
+      'imagen':'limpiezas_espirituales'
+    },
+    {
+      'label': 'Orientación personal',
+      'imagen':'orientacion_personal'
+    },
+    {
+      'label': 'Lectura general',
+      'imagen':'lectura_general'
+    },
+  ]
+}
