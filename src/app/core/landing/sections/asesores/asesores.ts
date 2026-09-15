e2a3cc60214f205fd2a4e941aa510f5096774864
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-09-15 13:37:30 -0400

    aasesores

--- src/app/core/landing/sections/asesores/asesores.ts
+++ src/app/core/landing/sections/asesores/asesores.ts
@@ -1,9 +1,16 @@
+import { CurrencyPipe } from '@angular/common';
+import { httpResource } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { IAdvisor } from '@core/interfaces';
 
 @Component({
   selector: 'app-asesores',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './asesores.html',
   styleUrl: './asesores.css',
 })
-export class Asesores {}
+export class Asesores {
+  private readonly url = `${API_URL}/v1/dashboard/advisors`;
+
+  protected resource = httpResource<IAdvisor[]>(() => this.url);
+}
