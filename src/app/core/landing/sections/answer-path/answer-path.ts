fa88e02376e0ba2d8619c1a32895e7f690cf6184
deepcompanykg <frontend.kg@deepcompany.com>
2026-09-15 20:11:02 -0400

    fix

--- src/app/core/landing/sections/answer-path/answer-path.ts
+++ src/app/core/landing/sections/answer-path/answer-path.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './answer-path.html',
   styleUrl: './answer-path.css',
 })
-export class AnswerPath {}
+export class AnswerPath {
+  protected readonly answerPath = [
+    { title: 'Pregunta', dscription: 'Cuéntanos qué quieres descubir.', icon: 'zoom' },
+    { title: 'Encuentra', dscription: 'Conecta con el experto indicado para tí.', icon: 'users' },
+    { title: 'Descubre', dscription: 'Realiza tu consulta por chat o llamada.', icon: 'message' },
+  ];
+}
